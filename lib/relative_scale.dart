1fcad0a876ff941b5769c4f6a28f21828482b74d
xamantra <xamantrx@gmail.com>
2020-01-18 22:37:18 +0800

    library description.

--- lib/relative_scale.dart
+++ lib/relative_scale.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// RelativeScaler is a simple and custom sizing system for flutter widgets to achieve the same physical sizes across different devices.
 library relative_scale;
 
 export 'src/relative_scale_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
